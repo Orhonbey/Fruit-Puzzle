bad238173c5deaf885fd6f65099f6d58aafb36b5
sunal <sunal.orhon@gmail.com>
2020-05-07 18:32:13 +0300

    first Project Commit

---
+++ Assets/JellyShader/Scripts/JellyClickReceiver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyClickReceiver : MonoBehaviour
+{
+    RaycastHit hit;
+    Ray clickRay;
+    Renderer modelRenderer;
+    public float timeFix;
+    public float maxTimeCount;
+    float currentTime;
+    // Use this for initialization
+    void Start()
+    {
+        modelRenderer = GetComponent<MeshRenderer>();
+        currentTime = timeFix;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime >= maxTimeCount)
+        {
+            currentTime = timeFix;
+        }
+        currentTime += Time.deltaTime;
+        modelRenderer.material.SetFloat("_ControlTime", currentTime);
+    }
+}
