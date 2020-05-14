2ff89ffe216d12358f91b243d2ca3c865dfc6eb5
sunal <sunal.orhon@gmail.com>
2020-05-14 10:22:34 +0300

    Effect ve Oyun Sonu kamere ve Ufak animasyon işlemelri Koda Döküldü .

---
+++ Assets/AFruit-Puzzle/Script/PlatformManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public static PlatformManager ins;
+    public List<Platform> platforms;
+    [HideInInspector]
+    public Platform active;
+    private void Awake()
+    {
+        ins = this;
+        active = platforms.Find(x => x.gameObject.activeSelf);
+    }
+}
